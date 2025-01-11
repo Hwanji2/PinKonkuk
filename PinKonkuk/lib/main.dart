2f89876f63beeff424c258e85354d286e7af5e83
danji404 <157474285+danji404@users.noreply.github.com>
2025-01-12 05:50:12 +0900

    ui 수정

--- PinKonkuk/lib/main.dart
+++ PinKonkuk/lib/main.dart
@@ -6,15 +6,15 @@ import 'dart:io';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 void main() {
-  // 데스크톱 환경에서 sqflite 초기화
-  sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
+
+// 데스크톱 환경에서 sqflite 초기화
+  sqfliteFfiInit(); // sqflite_common_ffi 초기화
+  databaseFactory = databaseFactoryFfi; // databaseFactory 설정
   runApp(const MyApp());
 }
-
 Future<Database> _initDB() async {
   String dbPath = await getDatabasesPath();
   String path = join(dbPath, 'pins.db');
@@ -24,7 +24,7 @@ Future<Database> _initDB() async {
     path,
     version: 1,
     onCreate: (db, version) async {
-      print('DB 테이블 생성 완료');
+      print('DB 테이블 생성 완료'); // 디버그 출력
       await db.execute('''
         CREATE TABLE pins (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -39,37 +39,6 @@ Future<Database> _initDB() async {
       ''');
     },
   );
-}
-
-class DBHelper {
-  static final DBHelper _instance = DBHelper._internal();
-  static Database? _database;
-
-  DBHelper._internal();
-
-  factory DBHelper() => _instance;
-
-  Future<Database> get database async {
-    if (_database != null) return _database!;
-    _database = await _initDB();
-    return _database!;
-  }
-
-  Future<void> insertPin(Map<String, dynamic> pin) async {
-    final db = await database;
-    int result = await db.insert('pins', pin);
-    print('DB에 핀 추가: $pin, 결과: $result');
-  }
-
-  Future<List<Map<String, dynamic>>> getPins() async {
-    final db = await database;
-    return await db.query('pins');
-  }
-
-  Future<void> deletePin(int id) async {
-    final db = await database;
-    await db.delete('pins', where: 'id = ?', whereArgs: [id]);
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -88,6 +57,62 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// DBHelper 클래스: SQLite 데이터베이스 초기화 및 CRUD 기능 제공
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+  static Database? _database;
+
+  DBHelper._internal();
+
+  factory DBHelper() => _instance;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    String dbPath = await getDatabasesPath();
+    String path = join(dbPath, 'pins.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE pins (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            note TEXT,
+            placeName TEXT,
+            latitude REAL,
+            longitude REAL,
+            category TEXT,
+            imagePath TEXT
+          )
+        ''');
+      },
+    );
+  }
+  Future<void> insertPin(Map<String, dynamic> pin) async {
+    final db = await database;
+    int result = await db.insert('pins', pin);
+    print('DB에 핀 추가: $pin, 결과: $result'); // 디버그 출력
+  }
+
+
+  Future<List<Map<String, dynamic>>> getPins() async {
+    final db = await database;
+    return await db.query('pins');
+  }
+
+  Future<void> deletePin(int id) async {
+    final db = await database;
+    await db.delete('pins', where: 'id = ?', whereArgs: [id]);
+  }
+}
+
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
@@ -99,19 +124,14 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
   Position? _currentPosition;
   List<Map<String, dynamic>> _journalEntries = [];
   final DBHelper _dbHelper = DBHelper();
-  // 검색어 변수 추가
-  String _searchQuery = '';
-  // 메인 카테고리 목록
   List<String> _mainCategories = [
     'All', '새내기', '맛집', '스터디', '건축', '예술', '공학', '상경', '인문', '놀거리', '안전'
   ];
-
-  // 각 카테고리에 대한 서브 카테고리 정의
   Map<String, List<String>> _subCategories = {
     'All': ['자유', 'wifi', '학생회관'],
     '새내기': ['학교 투어', '필수 코스', '박물관', '입학정보관', '행정관'],
     '놀거리': ['노래방', '보드게임카페', '파티', '헌팅', '영화관', '운동장', 'PC방'],
-    '맛집': ['한식', '양식', '간식', '디저트'],
+    '맛집': ['한가한', '가성비', '간식', '패스트푸드', '디저트', '한식', '중식', '일식', '양식', '학식', '테마'],
     '스터디': ['스터디룸', '카페', 'K-CUBE', '강의실', '도서관'],
     '건축': ['전시회', '외관', '조경', '실내', '건축관', '캠퍼스'],
     '예술': ['전시', '공연', '공방', '예술문화관'],
@@ -131,69 +151,147 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
   void initState() {
     super.initState();
     _getCurrentLocation();
+    _startLocationCheck();
     _loadPinsFromDB();
+
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _fadeAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   bool get wantKeepAlive => true;
 
   void _getCurrentLocation() async {
-    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       print('위치 서비스가 비활성화되어 있습니다.');
-      return;
+      return Future.error('위치 서비스가 비활성화되어 있습니다.');
     }
 
-    LocationPermission permission = await Geolocator.checkPermission();
+    permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
         print('위치 권한이 거부되었습니다.');
-        return;
+        return Future.error('위치 권한이 거부되었습니다.');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
       print('위치 권한이 영구적으로 거부되었습니다.');
-      return;
+      return Future.error('위치 권한이 영구적으로 거부되었습니다.');
     }
 
     Position position = await Geolocator.getCurrentPosition();
-    print('현재 위치: Lat=${position.latitude}, Lng=${position.longitude}');
+    print('현재 위치: Lat=${position.latitude}, Lng=${position.longitude}'); // 디버그 출력
     setState(() {
       _currentPosition = position;
     });
   }
 
+  void _startLocationCheck() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      _getCurrentLocation();
+      _checkForMatchingEntries();
+    });
+  }
+  void _checkForMatchingEntries() {
+    if (_currentPosition == null) return;
+
+    double thresholdDistance = 0.001; // 약 1m 이내의 거리
+
+    setState(() {
+      _journalEntries = _journalEntries.where((entry) {
+        // "자유" 선택 시 모든 항목 표시
+        if (_selectedSubCategory == '자유') {
+          return true;
+        }
+
+        // 카테고리와 위치가 일치하는 항목만 표시
+        if (_selectedMainCategory != 'All' &&
+            entry['category'] != _selectedSubCategory) {
+          return false;
+        }
+
+        double distance = (entry['latitude'] - _currentPosition!.latitude).abs() +
+            (entry['longitude'] - _currentPosition!.longitude).abs();
+        return distance < thresholdDistance;
+      }).toList();
+    });
+  }
+
+
   Future<void> _loadPinsFromDB() async {
     List<Map<String, dynamic>> pins = await _dbHelper.getPins();
-    print('DB에서 불러온 핀 데이터: $pins');
+    print('DB에서 불러온 핀 데이터: $pins'); // 디버그 출력
     setState(() {
       _journalEntries = pins;
     });
   }
 
-  void _addNewPin(Map<String, dynamic> pin) async {
-    await _dbHelper.insertPin(pin);
-    await _loadPinsFromDB();
+  Future<Database> _initDB() async {
+    String dbPath = await getDatabasesPath();
+    String path = join(dbPath, 'pins.db');
+    print('DB 경로: $path'); // 디버그 출력
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        print('DB 테이블 생성');
+        await db.execute('''
+        CREATE TABLE pins (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT,
+          note TEXT,
+          placeName TEXT,
+          latitude REAL,
+          longitude REAL,
+          category TEXT,
+          imagePath TEXT
+        )
+      ''');
+      },
+    );
   }
+
+  void _addNewPin(Map<String, dynamic> pin) async {
+    await _dbHelper.insertPin(pin); // DB에 핀 추가
+    await _loadPinsFromDB();        // DB에서 데이터를 다시 불러와 UI 갱신
+  }
+
 
   void _deletePin(int index) async {
     int id = _journalEntries[index]['id'];
     await _dbHelper.deletePin(id);
-    await _loadPinsFromDB();
+    _loadPinsFromDB(); // 삭제 후 DB에서 다시 로드하여 UI 갱신
   }
 
-  void _navigateToAddEntry(BuildContext context) {
+  void _navigateToAddEntry(BuildContext buildContext) {
+    print('Add Entry 버튼이 눌렸습니다.');
     Navigator.push(
-      context,
+      buildContext, // 상위에서 전달된 BuildContext 사용
       MaterialPageRoute(
         builder: (context) => AddEntryPage(
           currentPosition: _currentPosition,
           mainCategories: _mainCategories,
-          subCategories: _subCategories, // 기존에 정의된 _subCategories 사용
+          subCategories: _subCategories,
           onSave: _addNewPin,
-          parentContext: context,  // parentContext 전달
+          parentContext: buildContext, // BuildContext 전달
         ),
       ),
     );
@@ -202,20 +300,61 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
 
   @override
   Widget build(BuildContext context) {
-    super.build(context);
+    super.build(context); // AutomaticKeepAliveClientMixin을 사용할 때 필요
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PinKonkuk'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () => _showSearchDialog(context),
-          ),
-        ],
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Pin', style: TextStyle(color: Colors.pink, fontWeight: FontWeight.bold)),
+            Text('Konkuk', style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold)),
+          ],
+        ),
+        centerTitle: true,
       ),
       body: Column(
         children: [
-          _buildStatistics(),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: _mainCategories.map((mainCategory) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ChoiceChip(
+                    label: Text(mainCategory),
+                    selected: _selectedMainCategory == mainCategory,
+                    onSelected: (bool selected) {
+                      setState(() {
+                        _selectedMainCategory = mainCategory;
+                        _selectedSubCategory = _subCategories[mainCategory]!.first;
+                      });
+                    },
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          const SizedBox(height: 10),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: _subCategories[_selectedMainCategory]!.map((subCategory) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ChoiceChip(
+                    label: Text(subCategory),
+                    selected: _selectedSubCategory == subCategory,
+                    onSelected: (bool selected) {
+                      setState(() {
+                        _selectedSubCategory = subCategory;
+                      });
+                    },
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          const SizedBox(height: 10),
           Expanded(
             child: _currentPosition == null
                 ? const Center(child: CircularProgressIndicator())
@@ -223,18 +362,22 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
               itemCount: _journalEntries.length,
               itemBuilder: (context, index) {
                 var entry = _journalEntries[index];
-                return Card(
-                  margin: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    leading: entry['imagePath'] != null && entry['imagePath'].isNotEmpty
-                        ? Image.file(File(entry['imagePath']), width: 50, height: 50, fit: BoxFit.cover)
-                        : const Icon(Icons.location_pin, size: 50),
-                    title: Text(entry['placeName']),
-                    subtitle: Text(entry['note'] ?? ''),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete, color: Colors.red),
-                      onPressed: () => _deletePin(index),
-                    ),
+                return ListTile(
+                  title: Text(entry['title']),
+                  subtitle: Text(
+                    '${entry['placeName']} - ${entry['category']}\nLat: ${entry['latitude']}, Lng: ${entry['longitude']}',
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ViewEntryPage(entry: entry),
+                      ),
+                    );
+                  },
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _deletePin(index),
                   ),
                 );
               },
@@ -244,74 +387,13 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _navigateToAddEntry(context),
-        child: const Icon(Icons.add),
+        tooltip: 'Add Entry',
+        backgroundColor: Colors.pink,
+        child: const Icon(Icons.pin_drop),
       ),
     );
   }
-
-  Widget _buildStatistics() {
-    int totalPins = _journalEntries.length;
-    int categoryPins = _journalEntries.where((entry) => entry['category'] == _selectedMainCategory).length;
-
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildStatCard('총 핀 개수', totalPins.toString()),
-          _buildStatCard('현재 카테고리', categoryPins.toString()),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildStatCard(String title, String value) {
-    return Card(
-      margin: const EdgeInsets.all(8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
-            const SizedBox(height: 8),
-            Text(value, style: const TextStyle(fontSize: 20)),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _showSearchDialog(BuildContext buildContext) {
-    showDialog(
-      context: buildContext, // 전달받은 BuildContext 사용
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('핀 검색'),
-          content: TextField(
-            onChanged: (value) {
-              setState(() {
-                _searchQuery = value;
-                _journalEntries = _journalEntries
-                    .where((entry) => entry['placeName'].contains(_searchQuery))
-                    .toList();
-              });
-            },
-            decoration: const InputDecoration(hintText: '장소 이름으로 검색'),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(buildContext),
-              child: const Text('닫기'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
 }
-
 
 class AddEntryPage extends StatefulWidget {
   final Position? currentPosition;
